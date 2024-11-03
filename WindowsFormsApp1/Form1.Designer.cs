1d6aa59e00aeb437b0fd2c1873e09690b5af0585
Andryk Leon <andrykleoncorreos@gmail.com>
2024-11-03 11:53:40 -0700

    Commit inicial

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.c1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAceptar.Location = new System.Drawing.Point(628, 13);
+            this.btnAceptar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(155, 39);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese el texto a analizar:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.c1,
+            this.c2,
+            this.c3,
+            this.c4});
+            this.dataGridView1.Location = new System.Drawing.Point(125, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(553, 235);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // c1
+            // 
+            this.c1.HeaderText = "No";
+            this.c1.MinimumWidth = 6;
+            this.c1.Name = "c1";
+            this.c1.Width = 125;
+            // 
+            // c2
+            // 
+            this.c2.HeaderText = "No.Linea";
+            this.c2.MinimumWidth = 6;
+            this.c2.Name = "c2";
+            this.c2.Width = 125;
+            // 
+            // c3
+            // 
+            this.c3.HeaderText = "Cadena";
+            this.c3.MinimumWidth = 6;
+            this.c3.Name = "c3";
+            this.c3.Width = 125;
+            // 
+            // c4
+            // 
+            this.c4.HeaderText = "Tipo";
+            this.c4.MinimumWidth = 6;
+            this.c4.Name = "c4";
+            this.c4.Width = 125;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.Pink;
+            this.btnLimpiar.Location = new System.Drawing.Point(449, 13);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(155, 39);
+            this.btnLimpiar.TabIndex = 0;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(767, 198);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 263);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(804, 260);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(804, 523);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAceptar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Analizador Léxico PR06";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int edo, cont, tam, car, ultimoEdo, preUltimoEdo, contadorLineas, contadorCadenas;
+        string[] txt;
+        int[,] tt = { {0, 0, 0, 1, 0, 0, 0},
+                      {7, 0, 3, 1, 5, 2, 0},
+                      {7, 0, 0, 7, 7, 7, 0},
+                      {7, 7, 7, 4, 7, 7, 0},
+                      {7, 0, 7, 4, 5, 2, 0},
+                      {7, 0, 0, 6, 7, 7, 0},
+                      {7, 0, 0, 6, 7, 7, 0},
+                      {7, 0, 7, 7, 7, 7, 0}};
+        string[] tipoCadena = { "Natural", "Porcentaje", "Real", "Exponencial"};
+
+        string salida = "";
+
+        List<int> estadosDeAceptacion = new List<int> { 1, 2, 4, 6};
+        List<string> cadenaAMostrar = new List<string>();
+        List<char> uno = new List<char> {';', '(', ')'};
+        List<char> dos = new List<char> { ',', '.'};
+        List<char> tres = new List<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        List<char> seis = new List<char> { ' ', '\n' };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            dataGridView1.Rows.Clear();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            string cadenas = richTextBox1.Text;
+
+            realizarAutomata(cadenas);
+        }
+        private void realizarAutomata(string cadenasPorLineas)
+        {
+            string palabra = "";
+            char[] cadena = cadenasPorLineas.ToCharArray();
+            edo = 0; cont = 0; ultimoEdo = 0; preUltimoEdo = 0; contadorLineas = 1; contadorCadenas = 1; tam = cadena.Length;
+
+            while (cont < tam)
+            {
+                car = SacarCar(cadena[cont]);
+                edo = tt[edo, car];
+
+                if (edo != 0)
+                    cadenaAMostrar.Add(cadena[cont].ToString());
+
+                ImprimirFila(edo, palabra, cadena);
+
+                cont++;
+                preUltimoEdo = ultimoEdo;
+                ultimoEdo = edo;
+            }
+        }
+
+        private int SacarCar(char caracter)
+        {
+            int resultado = 999;
+
+            if (uno.Contains(caracter))
+                resultado = 1;
+
+            if (dos.Contains(caracter))
+                resultado = 2;
+
+            if (tres.Contains(caracter))
+                resultado = 3;
+
+            if (caracter == 'E')
+                resultado = 4;
+
+            if (caracter == '%')
+                resultado = 5;
+
+            if (seis.Contains(caracter))
+            {
+                resultado = 6;
+                if(caracter == '\n')
+                    contadorLineas++;
+            }
+
+            if (resultado == 999)
+                resultado = 0;
+
+            return resultado;
+        }
+        private string SacarTipo(int estado)
+        {
+            string resultado = "Invalido";
+
+            if(estadosDeAceptacion.Contains(estado))
+            {
+                switch(estado)
+                {
+                    case 1:
+                        resultado = tipoCadena[0];
+                        break;
+
+                    case 2:
+                        resultado = tipoCadena[1];
+                        break;
+
+                    case 4:
+                        resultado = tipoCadena[2];
+                        break;
+
+                    case 6:
+                        resultado = tipoCadena[3];
+                        break;
+                }
+            }
+
+            return resultado;
+        }
+
+        private void ImprimirFila (int edo, string palabra, char[] cadena)
+        {
+            if ((edo == 0) && cadenaAMostrar.Count > 0)
+            {
+                string tipo = "";
+                palabra = "";
+
+                foreach (var letra in cadenaAMostrar)
+                {
+                    palabra = palabra + letra;
+                }
+
+                if (cont > 0)
+                {
+                    if (dos.Contains(cadena[cont - 1]))
+                    {
+                        palabra = palabra.Remove(palabra.Length - 1);
+                        tipo = SacarTipo(preUltimoEdo);
+                    }
+                    else
+                        tipo = SacarTipo(ultimoEdo);
+                }
+                else
+                    tipo = SacarTipo(ultimoEdo);
+
+                dataGridView1.Rows.Add(contadorCadenas, contadorLineas, palabra, tipo);
+                contadorCadenas++;
+                cadenaAMostrar.Clear();
+            }
+        }
+    }
+}
